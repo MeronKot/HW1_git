1b1676ea8c26a532d9c8b367a148d8997dd15fda
meronkot <meron.kotler@gmail.com>
2016-11-12 15:02:43 +0200

    Add done func onClick

--- app/src/main/java/com/example/dell/hw1/MainActivity.java
+++ app/src/main/java/com/example/dell/hw1/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.dell.hw1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.hw1.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void done(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.name);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 
 }
